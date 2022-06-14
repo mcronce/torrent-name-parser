4e2247b5c88715af6a39e07ae699ec4e8e94e576
Mike Cronce <mike@quadra-tec.net>
2022-06-13 22:58:20 -0400

    src/metadata/quality.rs:  cargo fmt

--- src/metadata/quality.rs
+++ src/metadata/quality.rs
@@ -4,30 +4,31 @@ use crate::error::Error;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Quality {
-	HDTV,
-	Cam,
-	BluRay,
-	TS,
-	HD,
-	DVD,
-	Web,
+    HDTV,
+    Cam,
+    BluRay,
+    TS,
+    HD,
+    DVD,
+    Web,
 }
 
 impl FromStr for Quality {
-	type Err = Error;
-	fn from_str(input: &str) -> Result<Self, Self::Err> {
-		match input {
-			"HDTV"|"PPV.HDTV"|"PDTV"|"PPV.PDTV"|"hdtv" => Ok(Self::HDTV),
-			"CAM"|"HDCAM"|"CamRip" => Ok(Self::Cam),
-			"BrRip"|"BRRip"|"BluRay"|"Bluray"|"bluRay"|"bluray" => Ok(Self::BluRay),
-			"TS" => Ok(Self::TS),
-			"WEB"|"WEB-"|"WEB-DL"|"WEBDL"|"PPV WEB"|"PPV WEB-"|"PPV WEB-DL"|"PPV WEBDL" => Ok(Self::Web),
-			"WEB DVDRip"|"WEB- DVDRip"|"WEB-DL DVDRip"|"WEBDL DVDRip"|"PPV WEB DVDRip"|"PPV WEB- DVDRip"|"PPV WEB-DL DVDRip"|"PPV WEBDL DVDRip" => Ok(Self::Web),
-			"WEBRip"|"WEBrip"|"WBBRip"|"WBBrip" => Ok(Self::Web),
-			"HDRip"|"HdRip" => Ok(Self::HD),
-			"DVDRip"|"DVDRiP"|"DVDRIP"|"DvDScr" => Ok(Self::DVD),
-			s => Err(Error::InvalidQuality(s.into()))
-		}
-	}
+    type Err = Error;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "HDTV" | "PPV.HDTV" | "PDTV" | "PPV.PDTV" | "hdtv" => Ok(Self::HDTV),
+            "CAM" | "HDCAM" | "CamRip" => Ok(Self::Cam),
+            "BrRip" | "BRRip" | "BluRay" | "Bluray" | "bluRay" | "bluray" => Ok(Self::BluRay),
+            "TS" => Ok(Self::TS),
+            "WEB" | "WEB-" | "WEB-DL" | "WEBDL" | "PPV WEB" | "PPV WEB-" | "PPV WEB-DL"
+            | "PPV WEBDL" => Ok(Self::Web),
+            "WEB DVDRip" | "WEB- DVDRip" | "WEB-DL DVDRip" | "WEBDL DVDRip" | "PPV WEB DVDRip"
+            | "PPV WEB- DVDRip" | "PPV WEB-DL DVDRip" | "PPV WEBDL DVDRip" => Ok(Self::Web),
+            "WEBRip" | "WEBrip" | "WBBRip" | "WBBrip" => Ok(Self::Web),
+            "HDRip" | "HdRip" => Ok(Self::HD),
+            "DVDRip" | "DVDRiP" | "DVDRIP" | "DvDScr" => Ok(Self::DVD),
+            s => Err(Error::InvalidQuality(s.into())),
+        }
+    }
 }
-

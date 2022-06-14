13170202fe8e0e9412f527057b935b7698576b94
Mike Cronce <mike@quadra-tec.net>
2022-06-13 22:57:08 -0400

    src/error.rs:  cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,13 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-	#[error("Couldn't find a title; matches: {0:?}")]
-	Match(Vec<(&'static str, Option<String>)>),
-	#[error("Invalid resolution: {0}")]
-	InvalidResolution(String),
-	#[error("Invalid quality: {0}")]
-	InvalidQuality(String),
-	#[error("Invalid codec: {0}")]
-	InvalidCodec(String),
-	#[error("Invalid audio: {0}")]
-	InvalidAudio(String)
+    #[error("Couldn't find a title; matches: {0:?}")]
+    Match(Vec<(&'static str, Option<String>)>),
+    #[error("Invalid resolution: {0}")]
+    InvalidResolution(String),
+    #[error("Invalid quality: {0}")]
+    InvalidQuality(String),
+    #[error("Invalid codec: {0}")]
+    InvalidCodec(String),
+    #[error("Invalid audio: {0}")]
+    InvalidAudio(String),
 }
-
